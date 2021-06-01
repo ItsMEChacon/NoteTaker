e6e3c6a0ac0ca79bc30bb8e4672ff59cb3901c09
Manny Chacon <chmanuel2030@gmail.com>
2021-05-31 19:54:45 -0700

    homework folder starter code

---
+++ public/assets/js/server.js
@@ -0,0 +1,26 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//Data
+
+
+// Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+app.get('/notes.html', (req, res) => res.sendFile(path.join(__dirname, '../notes.html')));
+
+
+
+
+
+// Listener
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
